f3ece05b86ad0a48e2700a3e9adfa43f1fc99da3
lore1379 <47046432+lore1379@users.noreply.github.com>
2022-09-13 11:21:41 +0200

    Add files via upload

---
+++ DESdecrypter/DictionaryAttack.cpp
@@ -0,0 +1,184 @@
+//
+// Created by lorenzo on 29/08/22.
+//
+
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <crypt.h>
+#include <string.h>
+#include <iostream>
+#include <chrono>
+#include <omp.h>
+#include <cmath>
+#include "DictionaryAttack.h"
+
+using namespace std;
+
+DictionaryAttack::DictionaryAttack(vector<string> passwordsToDecrypt, string salt) {
+    this->salt = salt;
+    //riserva la capacità del vettore per contenere i dati senza riallocarli dinamicamente
+    encryptedPasswords.reserve(passwordsToDecrypt.size());
+
+    for (string& p : passwordsToDecrypt){
+        encryptedPasswords.push_back(crypt(p.c_str(), salt.c_str()));
+    }
+
+    //file stream in input
+    ifstream dictionary;
+    dictionary.open("/home/lorenzo/ParallelComputing/DESdecrypter/dict.txt");
+    string line;
+
+    while (getline(dictionary, line)) {
+        dictionaryVector.push_back(line);
+    }
+
+    dictionary.close();
+}
+
+vector<long> DictionaryAttack::sequential(int runsPerPassword){
+    vector<long> executionTimes;
+    executionTimes.reserve(encryptedPasswords.size());
+    vector<long> runTimes;
+    runTimes.reserve((unsigned long)runsPerPassword);
+
+    for (string& wantedPassword : encryptedPasswords){
+        runTimes.clear();
+
+        for (int i = 0; i < runsPerPassword; i++) {
+            auto start = chrono::steady_clock::now();
+
+            for (string& p : dictionaryVector) {
+                string encryptionAttempt(crypt(p.c_str(), salt.c_str()));
+
+                if (wantedPassword == encryptionAttempt) {
+                    //cout << "Password found: " << p << " - From hash: " << encryptionAttempt << endl;
+                    break;
+                }
+            }
+
+            auto stop = chrono::steady_clock::now();
+            auto elapsed_time = chrono::duration_cast<chrono::nanoseconds>(stop - start);
+            runTimes.push_back(elapsed_time.count());
+        }
+
+        executionTimes.push_back(getMean(runTimes));
+    }
+
+    return executionTimes;
+}
+
+vector<long> DictionaryAttack::autoParallel(int runsPerPassword, int numberOfThreads) {
+
+    vector<long> executionTimes;
+    executionTimes.reserve(encryptedPasswords.size());
+    vector<long> runTimes;
+    runTimes.reserve((unsigned long)runsPerPassword);
+
+    for (string& wantedPassword : encryptedPasswords){
+        runTimes.clear();
+
+        for (int i = 0; i < runsPerPassword; i++) {
+
+            volatile bool passFound = false;
+            auto start = chrono::steady_clock::now();
+
+            #pragma omp parallel num_threads(numberOfThreads)
+            {
+                // crypt_r is threadsafe, threads not contending crypt func
+                struct crypt_data data;
+                data.initialized = 0;
+
+                #pragma omp for firstprivate(dictionaryVector)
+                for (int i = 0; i < dictionaryVector.size(); i++) {
+                    if (passFound) continue;
+
+                    char *encryptionAttempt = crypt_r(dictionaryVector[i].c_str(), salt.c_str(), &data);
+
+                    if (strcmp(encryptionAttempt, wantedPassword.c_str()) == 0) {
+                        passFound = true;
+                    }
+                }
+            }
+
+            auto stop = chrono::steady_clock::now();
+            auto elapsed_time = chrono::duration_cast<chrono::nanoseconds>(stop - start);
+            runTimes.push_back(elapsed_time.count());
+        }
+
+        executionTimes.push_back(getMean(runTimes));
+    }
+
+    return executionTimes;
+
+}
+
+vector<long> DictionaryAttack::manualParallel(int runsPerPassword, int numberOfThreads) {
+    vector<long> executionTimes;
+    executionTimes.reserve(encryptedPasswords.size());
+    vector<long> runTimes;
+    runTimes.reserve((unsigned long)runsPerPassword);
+
+    for (string& wantedPassword : encryptedPasswords) {
+        runTimes.clear();
+
+        for (int i = 0; i < runsPerPassword; i++) {
+            int chunkPerThread = static_cast<int>(ceil((double)dictionaryVector.size() / (double)numberOfThreads));
+            volatile bool passFound = false;
+
+            auto start = chrono::steady_clock::now();
+
+            #pragma omp parallel num_threads(numberOfThreads) shared(passFound)
+            {
+                struct crypt_data data;
+                data.initialized = 0;
+
+                int numThread = omp_get_thread_num();
+
+                for (int i = chunkPerThread * numThread; i < (numThread + 1) * chunkPerThread; i++) {
+                    if (!passFound && i < dictionaryVector.size()) {
+
+                        char *encryptionAttempt = crypt_r(dictionaryVector[i].c_str(), salt.c_str(), &data);
+
+                        if (strcmp(encryptionAttempt, wantedPassword.c_str()) == 0) {
+                            passFound = true;
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            auto stop = chrono::steady_clock::now();
+            auto elapsed_time = chrono::duration_cast<chrono::nanoseconds>(stop - start);
+            runTimes.push_back(elapsed_time.count());
+        }
+
+        executionTimes.push_back(getMean(runTimes));
+    }
+
+    return executionTimes;
+
+}
+
+long DictionaryAttack::getMean(vector<long> values) {
+    long sum = 0;
+
+    for (long v : values) {
+        sum += v;
+    }
+
+    return sum / values.size();
+}
+
+vector<float> DictionaryAttack::computeSpeedUp(vector<long> sequentialTimes, vector<long> parallelTimes) {
+    vector<float> speedups;
+    speedups.reserve(sequentialTimes.size());
+
+    for (int i=0; i < sequentialTimes.size(); i++) {
+        speedups.push_back((float)sequentialTimes[i] / (float)parallelTimes[i]);
+    }
+
+    return speedups;
+}
---
+++ DESdecrypter/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <unistd.h>
+#include "DictionaryAttack.h"
+
+using namespace std;
+
+void printSpeedup(vector<string> passwords, vector<long> sequentialTimes, vector<long> parallelTimes,
+                  vector<float> speedup, string method);
+
+int main(int argc, char* argv[]) {
+    // da rivedere meglio questa richiesta input
+    if (argc != 4) {
+        if (argc < 2) {
+            cout << "Too few arguments!" << endl;
+            return 1;
+        }
+    }
+
+    int runsPerPassword;
+    runsPerPassword = atoi(argv[1]);
+
+    if (runsPerPassword <= 0) {
+        cout << "Number of runs must be a positive integer" << endl;
+        return 1;
+    }
+
+    vector<string> passwordsToDecrypt = {"aaghrslq", "aaghyjat", "aagiezpy", "aagilpmw"};
+    string salt = "k2";
+
+    DictionaryAttack dictionaryAttack(passwordsToDecrypt, salt);
+
+    vector<long> sequentialTimes, autoParallelTimes, manualParallelTimes;
+    vector<float> autoSpeedup, manualSpeedup;
+    vector<int> numberOfThreads = {4}; //,3,4,5,6,7,8,9,10,20,40,60,80,100,200,500,1000,5000,10000};
+
+    sequentialTimes = dictionaryAttack.sequential(runsPerPassword);
+
+    for (int t : numberOfThreads) {
+        cout << endl;
+        cout << "Number of threads used: " << t << endl;
+
+        autoParallelTimes = dictionaryAttack.autoParallel(runsPerPassword, t);
+        autoSpeedup = dictionaryAttack.computeSpeedUp(sequentialTimes, autoParallelTimes);
+        printSpeedup(passwordsToDecrypt, sequentialTimes, autoParallelTimes, autoSpeedup, "automatic parallel");
+        usleep(5000000);
+
+        manualParallelTimes = dictionaryAttack.manualParallel(runsPerPassword, t);
+        manualSpeedup = dictionaryAttack.computeSpeedUp(sequentialTimes, manualParallelTimes);
+        printSpeedup(passwordsToDecrypt, sequentialTimes, manualParallelTimes, manualSpeedup, "manual parallel");
+        usleep(5000000);
+
+    }
+
+    return 0;
+}
+
+void printSpeedup(vector<string> passwords, vector<long> sequentialTimes, vector<long> parallelTimes,
+        vector<float> speedup, string method) {
+
+    cout << "Using " << method << " method: " << endl;
+
+    for (int i = 0; i < passwords.size(); i++) {
+        cout << "Password #" << i+1 << ": " << passwords[i] << " - ";
+        cout << "Seq. time " << sequentialTimes[i] << "ns - Par. time " << parallelTimes[i] << " ns - ";
+        cout << "Speed up " << fixed << setprecision(7) << speedup[i] << endl;
+    }
+}
